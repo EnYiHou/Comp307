98b9ab68097c2ac19256062ff1a7af4f82bf6ffc
Course User <cs307-user@winter2026-comp307-group26.cs.mcgill.ca>
2026-04-14 00:54:13 -0400

    bogdan mongodb

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 
-const defaultMongoUri = 'mongodb://127.0.0.1:27017/comp307';
 
 export const connectDB = async () => {
+  const dbUser = process.env.MONGODB_USERNAME;
+  const dbPassword = process.env.MONGODB_PASSWORD;
+  const defaultMongoUri = `mongodb+srv://${dbUser}:${dbPassword}@comp307-berk.mj9lurk.mongodb.net/?appName=COMP307-BERK`;
   const mongoUri = process.env.MONGODB_URI || defaultMongoUri;
 
   mongoose.set('strictQuery', true);
