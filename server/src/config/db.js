54ce57c3a0217a7e043e48af0a116cd2f06ed055
Course User <cs307-user@winter2026-comp307-group26.cs.mcgill.ca>
2026-04-14 01:08:38 -0400

    fix dotenv loading

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 export const connectDB = async () => {
   const dbUser = process.env.MONGODB_USERNAME;
   const dbPassword = process.env.MONGODB_PASSWORD;
   const defaultMongoUri = `mongodb+srv://${dbUser}:${dbPassword}@comp307-berk.mj9lurk.mongodb.net/?appName=COMP307-BERK`;
+  console.log('Connecting to MongoDB with URI:', defaultMongoUri);
   const mongoUri = process.env.MONGODB_URI || defaultMongoUri;
 
   mongoose.set('strictQuery', true);
