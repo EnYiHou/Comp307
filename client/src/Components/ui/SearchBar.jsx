405f5be19488a37684b68fa2473fcba1e51cf815
EnYi Hou <enyi.hou@gmail.com>
2026-04-24 15:11:46 -0400

    add search delay

--- client/src/Components/ui/SearchBar.jsx
+++ client/src/Components/ui/SearchBar.jsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from "react";
+
 export default function SearchBar({
   value,
   onChange,
   placeholder = "Search something",
 }) {
+  const [inputValue, setInputValue] = useState(value);
+  const DELAY = 1000;
+
+  useEffect(() => {
+    setInputValue(value);
+  }, [value]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(inputValue);
+    }, DELAY);
+
+    return () => clearTimeout(timeout);
+  }, [inputValue, onChange]);
+
   return (
     <input
       type="search"
-      value={value}
-      onChange={(e) => {
-        onChange(e.target.value);
-      }}
+      value={inputValue}
+      onChange={(e) => setInputValue(e.target.value)}
       placeholder={placeholder}
       className="search-bar"
     />
