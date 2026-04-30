862e9f61cb94dc540dc1208df2653a810ae2fe33
Railoou <bogdan.timercan@gmail.com>
2026-04-30 05:12:57 -0400

    removed old routes

--- server/src/app.js
+++ server/src/app.js
@@ -7,12 +7,8 @@ import { env } from "./config/env.js";
 import errorMiddleware from "./middleware/errorMiddleware.js";
 import authRoutes from "./routes/authRoutes.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
-import calendarRoutes from "./routes/calendarRoutes.js";
-import groupMeetingRoutes from "./routes/groupMeetingRoutes.js";
 import meetingRequestRoutes from "./routes/meetingRequestRoutes.js";
 import ownerRoutes from "./routes/ownerRoutes.js";
-import slotRoutes from "./routes/slotRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
 import dashboardRoutes from './routes/dashboardRoutes.js'
 import teamRoutes from './routes/teamRoutes.js';
 
@@ -43,13 +39,9 @@ app.use(morgan("dev"));
 app.use(express.static(clientDistPath));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
 app.use("/api/owners", ownerRoutes);
-app.use("/api/slots", slotRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/meeting-requests", meetingRequestRoutes);
-app.use("/api/group-meetings", groupMeetingRoutes);
-app.use("/api/calendar", calendarRoutes);
 
 app.use('/api/dashboard', dashboardRoutes);
 app.use("/api/teams", teamRoutes);
